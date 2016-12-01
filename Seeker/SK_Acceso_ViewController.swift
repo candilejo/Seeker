124cbeb0bed20f32f917f1345a3b488fc257b6ad
Jose Candilejo <Candilejo@MacBook-Air-de-Jose.local>
2016-12-01 17:01:41 +0100

    01/12/2016 - 17:00 	Comenzando version 2.0

---
+++ Seeker/SK_Acceso_ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SK_Acceso_ViewController.swift
+//  Seeker
+//
+//  Created by Jose Candilejo on 1/12/16.
+//  Copyright © 2016 Jose Candilejo. All rights reserved.
+//
+
+import UIKit
+
+class SK_Acceso_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
